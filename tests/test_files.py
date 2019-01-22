6aa927e90c5f304901e7f4772819fdf389f335b9
kk6 <hiro.ashiya@gmail.com>
2019-01-22 16:46:47 +0900

    Add some tests.

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,17 +1,47 @@
+import json
+from pathlib import Path
+
+import pytest
+import responses
 from poetrify.files import get_requires
 from poetrify.files import Pipfile
 from poetrify.files import RequirementsTxt
 
 
-def test_pipfile():
-    src = Pipfile("repos/foo/Pipfile")
+@pytest.fixture()
+def fixture():
+    return Path(__file__).parent / "fixtures" / "Pipfile"
+
+
+def test_pipfile(fixture):
+    src = Pipfile(fixture)
     packages, dev_packages = get_requires(src)
     assert packages == ["scrapy", "beautifulsoup4"]
     assert dev_packages == []
 
 
-def test_requirements():
-    src = RequirementsTxt("repos/requirements_txt/requirements.txt")
+def test_pipfile_if_not_exists():
+    with pytest.raises(FileNotFoundError):
+        Pipfile("foo")
+
+
+@responses.activate
+def test_requirements(fixture):
+    for name in ("django", "pytest"):
+        with open(fixture.with_name(f"{name}.json")) as f:
+            responses.add(
+                responses.GET,
+                f"https://pypi.org/pypi/{name}/json",
+                body=f.read(),
+                status=200,
+                content_type="application/json",
+            )
+    src = RequirementsTxt(fixture.with_name("requirements.txt"))
     packages, dev_packages = get_requires(src)
     assert packages == ["django", "pytest"]
     assert dev_packages == []
+
+
+def test_requirements_if_not_exists():
+    with pytest.raises(FileNotFoundError):
+        RequirementsTxt("bar")
